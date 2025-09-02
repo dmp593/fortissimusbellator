206045f7e2b30fbbcb117bad38392890ce4e29d9
Daniel Pinto <daniel.pinto@setwin.pt>
2025-09-02 23:30:51 +0100

    quiz admin

--- quiz/admin.py
+++ quiz/admin.py
@@ -1,22 +1,30 @@
 from django.contrib import admin
 
-from modeltranslation.admin import TranslationAdmin
+from modeltranslation.admin import (
+    TranslationAdmin, TranslationStackedInline
+)
+
 from .models import Question, Answer, AnswerWeight
+
+
+class AnswerStackedInline(TranslationStackedInline):
+    model = Answer
+    extra = 1
 
 
 @admin.register(Question)
 class QuestionAdmin(TranslationAdmin):
+    model = Question
     list_display = ('text', 'order')
     list_editable = ('order',)
     search_fields = ('text',)
     ordering = ('order',)
+    inlines = [AnswerStackedInline]
 
 
-class AnswerWeightAdmin(admin.StackedInline):
+class AnswerWeightStackedInline(admin.StackedInline):
     model = AnswerWeight
     extra = 1
-    fields = ('breed', 'weight')
-    ordering = ('-weight', 'breed__name',)
 
 
 @admin.register(Answer)
@@ -25,5 +33,4 @@ class AnswerAdmin(TranslationAdmin):
     list_editable = ('order',)
     search_fields = ('text',)
     ordering = ('question__order', 'order',)
-
-    inlines = [AnswerWeightAdmin]
+    inlines = [AnswerWeightStackedInline]
